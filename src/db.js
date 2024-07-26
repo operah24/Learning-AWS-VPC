28d03a0d4a046d37121a46e3162925ad863e6238
bantale <bantale@kobo360.com>
2024-07-26 16:26:51 +0100

    add db

--- src/db.js
+++ src/db.js
@@ -4,10 +4,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.RDS_HOST,
+  user: process.env.RDS_USER,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT,
+  database: 'test'
 });
 
 connection.connect((err) => {
